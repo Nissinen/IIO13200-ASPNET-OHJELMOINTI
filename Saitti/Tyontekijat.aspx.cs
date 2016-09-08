e8d3f7f58f37f2e68c9972f5f549aea60782cf13
H8593 <H8593@MEDI-426-WS-08>
2016-09-08 10:59:34 +0300

    Eka tunnin tuotokset

---
+++ Saitti/Tyontekijat.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Configuration; // web.configin lukemiseen
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tyontekijat : System.Web.UI.Page
+{
+    string xmlfilu;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //haetaan web.configista xml-tiedoston nimi
+        xmlfilu = ConfigurationManager.AppSettings["xmlfilu"];
+        lblMessages.Text = xmlfilu;
+    }
+
+    protected void btnHae_Click(object sender, EventArgs e)
+    {
+        //haetaan XML-data DataView-olioon, joka kytketään GridViewhen
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        DataView dv = new DataView();
+        ds.ReadXml(Server.MapPath(xmlfilu)); // huom mappath viittaa webbisaitille
+        dt = ds.Tables[0];
+        dv = dt.DefaultView;
+        gvData.DataSource = dv;
+        gvData.DataBind();
+    }
+}
