240179d3d0d191598fe827b74241100057347da4
Nissinen <frazento@hotmail.com>
2016-09-23 23:33:18 +0300

    Tehtava3 - valmis

---
+++ Tehtava3/Kalenteri.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Kalenteri.aspx.cs" Inherits="Kalenteri" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblThisDate" runat="server">Tänään on: </asp:Label>
+        <br />
+        <asp:Label ID="lblCurrentDate" runat="server">Valitse haluamasi päivä, valittu pvm: </asp:Label>
+        <asp:Label ID="lblSelectedDate" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="lblDifference" runat="server">Valitun päivän ja tämän päivän erotus on: </asp:Label>
+        <asp:Label ID="lblShow" runat="server"></asp:Label>
+        <br />
+        <asp:Button ID="decreaseYear" runat="server" Text="< Vuosi" OnClick="decreaseYear_Click" />
+        <asp:Button ID="addYear" runat="server" Text="Vuosi >" OnClick="addYear_Click" />
+        <asp:Calendar ID="Calendar1" OnSelectionChanged="Calendar1_SelectionChanged" runat="server"></asp:Calendar>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Tehtava3/Kalenteri.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Kalenteri : System.Web.UI.Page
+{
+    DateTime currentDate;
+    DateTime selectedDate;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack) { 
+        currentDate = DateTime.Today;
+        lblThisDate.Text = "Tänään on: " + currentDate.ToString("d");
+        }
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        currentDate = DateTime.Today;
+        selectedDate = Calendar1.SelectedDate;
+        TimeSpan result = selectedDate - currentDate;
+
+        int years = Convert.ToInt16(result.Days / 365);
+        int months = Convert.ToInt16((result.Days % 365) / 30);
+        int days = Convert.ToInt16((result.Days % 365) % 30);
+
+        lblThisDate.Text = "Tänään on: " + currentDate.ToString("d");
+        lblSelectedDate.Text = selectedDate.ToString("d");
+
+        lblShow.Text = Convert.ToString(years) + " Vuotta, " + Convert.ToString(months) + " Kuukautta, " + Convert.ToString(days) + " Päivää";
+
+    }
+
+    protected void decreaseYear_Click(object sender, EventArgs e)
+    {
+        selectedDate = Calendar1.SelectedDate;
+        if (selectedDate.Year <= 1)
+        {
+            selectedDate = DateTime.Today;
+        }
+        int day = Convert.ToInt16(selectedDate.Day);
+        int month = Convert.ToInt16(selectedDate.Month);
+        int year = Convert.ToInt16(selectedDate.Year -1);
+        Calendar1.VisibleDate = new DateTime(year, month, day);
+        Calendar1.SelectedDate = new DateTime(year, month, day);
+    }
+
+    protected void addYear_Click(object sender, EventArgs e)
+    {
+        selectedDate = Calendar1.SelectedDate;
+        if (selectedDate.Year <= 1)
+        {
+            selectedDate = DateTime.Today;
+        }
+        int day = Convert.ToInt16(selectedDate.Day);
+        int month = Convert.ToInt16(selectedDate.Month);
+        int year = Convert.ToInt16(selectedDate.Year + 1);
+        Calendar1.VisibleDate = new DateTime(year, month, day);
+        Calendar1.SelectedDate = new DateTime(year, month, day);
+    }
+}
