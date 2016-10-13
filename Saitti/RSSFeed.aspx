2e52b833eb91c5bf04499e968393d7deeefb2c60
H8593 <H8593@MEDI-426-WS-08>
2016-10-13 09:38:08 +0300

    Tunti tuotoksia osa 1

---
+++ Saitti/RSSFeed.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="RSSFeed.aspx.cs" Inherits="RSSFeed" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <asp:Button ID="btnGetFeeds" runat="server" Text="Hae Iltasanomat" OnClick="btnGetFeeds_Click" />
+    <asp:XmlDataSource ID="xdsFeedit" XPath="rss/channel/item" runat="server"></asp:XmlDataSource>
+    <asp:Literal ID="messages" runat="server"></asp:Literal>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+</asp:Content>
+
---
+++ Saitti/RSSFeed.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class RSSFeed : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnGetFeeds_Click(object sender, EventArgs e)
+    {
+        //asetetaan XmlDataSource poittaamaan Iltasanomien rss feediin
+        xdsFeedit.DataFile = @"http://www.iltasanomat.fi/rss/tuoreimmat.xml";
+        xdsFeedit.XPath = @"rss/channel/item";
+        GetFeeds();
+    }
+    protected void GetFeeds()
+    {
+        try
+        {
+            //hakee xml:n XMlDocument-olioon
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc = xdsFeedit.GetXmlDocument();
+            //rssfeedin title ja julkaisu aika
+            XmlNode node1 = xmlDoc.SelectSingleNode("/rss/channel");
+            string otsikko = node1["title"].InnerText;
+            string jaika = node1["pubDate"].InnerText;
+
+            XmlNodeList nodes = xmlDoc.SelectNodes("/rss/channel/item");
+
+            foreach (XmlNode item in nodes)
+            {
+
+            }
+
+            messages.Text = string.Format("<h1> {0} {1} </h1>", otsikko, jaika);
+        }
+        catch (Exception ex)
+        {
+            messages.Text = ex.Message;
+        }
+    }
+}
