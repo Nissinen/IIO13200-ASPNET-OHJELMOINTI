47cc21bf1bfefdf61b312279db59182d4471f43d
Nissinen <frazento@hotmail.com>
2016-10-17 00:58:14 +0300

    Teht8

---
+++ Tehtava8/Tehtava8.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Tehtava8.aspx.cs" Inherits="Tehtava8" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="CSS/style.css" rel="stylesheet" type="text/css" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="main">
+        <div class="right">
+            <asp:Repeater ID="Repeater1" runat="server" >
+              <ItemTemplate>
+                   <asp:Image ID="Image1" Width="150px" ImageUrl='<%# Container.DataItem  %>' runat="server" /> 
+             </ItemTemplate>
+             </asp:Repeater>
+        </div>
+        <div class="left">
+            <asp:ListBox CssClass="lb" ID="lbTeatterit" runat="server" OnSelectedIndexChanged="lbTeatterit_SelectedIndexChanged" AutoPostBack="True"></asp:ListBox>
+            <asp:ListView ID="ListView1" runat="server"></asp:ListView>
+            <br />
+            <asp:Label ID="errMsg" runat="server"></asp:Label>
+        </div>
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ Tehtava8/Tehtava8.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class Tehtava8 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (lbTeatterit.Items.Count < 1)
+        {
+            try
+            {
+                XmlDocument doc1 = new XmlDocument();
+                doc1.Load("http://www.finnkino.fi/xml/TheatreAreas/");
+                XmlElement root = doc1.DocumentElement;
+                XmlNodeList nodes = root.SelectNodes("/TheatreAreas/TheatreArea");
+                foreach (XmlNode node in nodes)
+                {
+                    lbTeatterit.Items.Add(new ListItem(node["Name"].InnerText, node["ID"].InnerText));
+                }
+                errMsg.Text = "";
+            }
+            catch (Exception ex)
+            {
+                errMsg.Text = ex.Message;
+            }
+        }
+    }
+
+    protected void lbTeatterit_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            List<String> photos = new List<string>();
+            DateTime thisDay = DateTime.Today;
+            string date = thisDay.Day.ToString() + "." + thisDay.Month.ToString() + "." + thisDay.Year.ToString();
+            XmlDocument doc1 = new XmlDocument();
+            string route = "http://www.finnkino.fi/xml/Schedule/?area=" + lbTeatterit.SelectedValue.ToString() + "&dt=" + date;
+            doc1.Load(route);
+            XmlElement root = doc1.DocumentElement;  
+            XmlNodeList nodes = root.SelectNodes("/Schedule/Shows/Show/Images/EventSmallImagePortrait");
+            foreach (XmlNode node in nodes)
+            {
+                    photos.Add(node.InnerText);
+            }
+            Repeater1.DataSource = photos;
+            Repeater1.DataBind();
+            errMsg.Text = "";
+        }
+        catch (Exception ex)
+        {
+            errMsg.Text = ex.Message;
+        }
+    }
+}
