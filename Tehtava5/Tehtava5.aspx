4c49a666e8686e533a65c24677c90e8a763c563a
Nissinen <frazento@hotmail.com>
2016-11-06 22:13:07 +0200

    Teht5 valmis

--- Tehtava5/Tehtava5.aspx
+++ Tehtava5/Tehtava5.aspx
@@ -12,9 +12,45 @@
         <asp:SqlDataSource ID="srcAsiakas" runat="server" ConnectionString="<%$ ConnectionStrings:DemoxOyConnectionString %>" SelectCommand="SELECT * FROM [asiakas]"></asp:SqlDataSource>
         <asp:Button ID="btnHaeKaikki" runat="server" Text="HaeKaikkiAsiakkaat" OnClick="btnHaeKaikki_Click" />
         <asp:DropDownList ID="ddlCountries" runat="server"></asp:DropDownList>
+        <asp:Button ID="btnHaeMaasta" runat="server" Text="Hae asiakkaat valitusta maasta" OnClick="btnHaeMaasta_Click" />
+        <asp:Button ID="btnHaeMaittain" runat="server" Text="Hae asiakkaat maittain" OnClick="btnHaeMaittain_Click" />
+        <asp:Button ID="btnLuo" runat="server" Text="Luo uusi asiakas" OnClick="btnLuo_Click" />
+        <br />
+        <asp:Panel ID="Panel1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Asiakastunnus: "></asp:Label>
+        <asp:TextBox ID="txtAsTunnus" Text="TEST12" runat="server"></asp:TextBox>
+
+        <asp:Label ID="Label2" runat="server" Text="Nimi: "></asp:Label>
+        <asp:TextBox ID="txtAsNimi" Text="Testaaja oy" runat="server"></asp:TextBox>
+
+        <asp:Label ID="Label3" runat="server" Text="Yhteyshenkilö: "></asp:Label>
+        <asp:TextBox ID="txtAsYhtHlo" Text="Testi Testaaja" runat="server"></asp:TextBox>
+
+        <asp:Label ID="Label4" runat="server" Text="Maa: "></asp:Label>
+        <asp:TextBox ID="txtMaa" Text="FIN" runat="server"></asp:TextBox>
+
+            <br />
+
+        <asp:Label ID="Label5" runat="server" Text="Posti Numero: "></asp:Label>
+        <asp:TextBox ID="txtPostiNro" Text="15230" runat="server"></asp:TextBox>
+
+        <asp:Label ID="Label6" runat="server" Text="Posti Toimipaikka: "></asp:Label>
+        <asp:TextBox ID="txtPostiTmp" Text="Seinäjoki" runat="server"></asp:TextBox>
+
+        <asp:Label ID="Label7" runat="server" Text="Ostot: "></asp:Label>
+        <asp:TextBox ID="txtOstot" Text="0" runat="server"></asp:TextBox>
+
+        <asp:Label ID="Label8" runat="server" Text="AsiointiVuosi"></asp:Label>
+        <asp:TextBox ID="txtAsVuosi" Text="2012" runat="server"></asp:TextBox>
+
+        <br />
+
+        <asp:Button ID="btnSend" runat="server" Text="Tallenna asiakas" OnClick="btnSend_Click" />
+        </asp:Panel>
+
         <asp:GridView ID="gvAsiakkaat" runat="server">
         </asp:GridView>
-        <asp:Label ID="lblMessages" runat="server" Text="Label"></asp:Label>
+        <asp:Label ID="lblMessages" runat="server" Text=""></asp:Label>  
     </div>
     </form>
 </body>
--- Tehtava5/Tehtava5.aspx.cs
+++ Tehtava5/Tehtava5.aspx.cs
@@ -12,6 +12,9 @@ public partial class Tehtava5 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack) {
+            Panel1.Visible = false;
+            DataTable dt = new DataTable();
         string ConnString = ConfigurationManager.ConnectionStrings["DemoxOyConnectionString"].ConnectionString;
         string select = "Select maa from asiakas";
         try
@@ -21,36 +24,35 @@ public partial class Tehtava5 : System.Web.UI.Page
 
             conn.Open();
             SqlDataAdapter da = new SqlDataAdapter(MyCommand);
-            DataTable dt = new DataTable();
+            
             da.Fill(dt);
 
-            ddlCountries.DataSource = dt;
-            ddlCountries.DataTextField = "maa";
             ddlCountries.DataBind();
-            /*        var result = from c in dt select new { c.id, c.lastname };
-                //määritellään dropdownlistille mitä se esittää
-                ddlCountries.SelectedIndex = -1;
-                ddlCountries.DataSource = result.ToList();
-                ddlCountries.DataTextField = "lastname";
-                ddlCountries.DataValueField = "id";
-                ddlCountries.DataBind();
-                ddlCountries.Items.Insert(0, string.Empty);*/
 
-            gvAsiakkaat.DataSource = dt;
-            gvAsiakkaat.DataBind();
             conn.Close();
             da.Dispose();
         }
         catch (Exception ex)
         {
-
+                lblMessages.Text = ex.Message;
+            }
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            if (!ddlCountries.Items.Contains(new ListItem(dt.Rows[i]["maa"].ToString())))
+            {
+                ddlCountries.Items.Add(Convert.ToString(dt.Rows[i]["maa"]));
+            }
+        }
+        ddlCountries.Items.Insert(0, string.Empty);
         }
     }
 
     protected void btnHaeKaikki_Click(object sender, EventArgs e)
     {
+        gvAsiakkaat.Visible = true;
+        lblMessages.Text = "";
         string ConnString = ConfigurationManager.ConnectionStrings["DemoxOyConnectionString"].ConnectionString;
-        string select = "Select * from asiakas";
+        string select = "Select * from asiakas ORDER by maa";
         try
         {
             SqlConnection conn = new SqlConnection(ConnString);
@@ -64,7 +66,104 @@ public partial class Tehtava5 : System.Web.UI.Page
         }
         catch (Exception ex)
         {
-            
+            lblMessages.Text = ex.Message;
         }
+    }
+
+    protected void btnHaeMaasta_Click(object sender, EventArgs e)
+    {
+        gvAsiakkaat.Visible = true;
+        lblMessages.Text = "";
+        string ConnString = ConfigurationManager.ConnectionStrings["DemoxOyConnectionString"].ConnectionString;
+        string select = "Select * from asiakas WHERE maa='" + ddlCountries.SelectedValue.ToString() + "'";
+        try
+        {
+            SqlConnection conn = new SqlConnection(ConnString);
+            SqlCommand MyCommand = new SqlCommand(select, conn);
+
+            conn.Open();
+            SqlDataReader MyReader = MyCommand.ExecuteReader();
+            gvAsiakkaat.DataSource = MyReader;
+            gvAsiakkaat.DataBind();
+            conn.Close();
+        }
+        catch (Exception ex)
+        {
+            lblMessages.Text = ex.Message;
+        }
+    }
+
+    protected void btnHaeMaittain_Click(object sender, EventArgs e)
+    {
+
+        gvAsiakkaat.Visible = false;
+        string ConnString = ConfigurationManager.ConnectionStrings["DemoxOyConnectionString"].ConnectionString;
+        string select = "Select * from asiakas ORDER by maa";
+        DataTable dt = new DataTable();
+        try
+        {
+            SqlConnection conn = new SqlConnection(ConnString);
+            SqlCommand MyCommand = new SqlCommand(select, conn);
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(MyCommand);
+
+            da.Fill(dt);
+
+            conn.Close();
+            da.Dispose();
+        }
+        catch (Exception ex)
+        {
+            lblMessages.Text = ex.Message;
+        }
+        string maa = Convert.ToString(dt.Rows[0]["maa"]);
+        string check = "";
+        lblMessages.Text = "";
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            string name = Convert.ToString(dt.Rows[i]["asnimi"]);
+            string yhthlo = Convert.ToString(dt.Rows[i]["yhteyshlo"]);
+            maa = Convert.ToString(dt.Rows[i]["maa"]);
+
+            if (check != Convert.ToString(dt.Rows[i]["maa"]))
+            {
+                lblMessages.Text += string.Format("<br> <h1> {1} </h1> {0} {2}", name, maa, yhthlo);
+            }
+            else
+            {
+                lblMessages.Text += string.Format("<br> {0} {1}", name, yhthlo);
+            }
+            check = Convert.ToString(dt.Rows[i]["maa"]);
+        }
+    }
+
+    protected void btnLuo_Click(object sender, EventArgs e)
+    {
+        Panel1.Visible = true;
+        gvAsiakkaat.Visible = false;
+    }
+
+    protected void btnSend_Click(object sender, EventArgs e)
+    {
+        string ConnString = ConfigurationManager.ConnectionStrings["DemoxOyConnectionString"].ConnectionString;
+        string insert = "Insert into asiakas(astunnus, asnimi, yhteyshlo, maa, postinro, postitmp, ostot, asvuosi) values('" + txtAsTunnus.Text + "','" + txtAsNimi.Text + "','" + txtAsYhtHlo.Text + "','" + txtMaa.Text +
+            "','" + txtPostiNro.Text + "','" + txtPostiTmp.Text + "','" + txtOstot.Text + "','" + txtAsVuosi.Text + "')";
+
+        try
+        {
+            SqlConnection conn = new SqlConnection(ConnString);
+            SqlCommand MyCommand = new SqlCommand(insert, conn);
+            SqlDataReader MyReader;
+            conn.Open();
+            MyReader = MyCommand.ExecuteReader();
+            conn.Close();
+            lblMessages.Text = "Palaute lähetetty onnistuneesti.";
+        }
+        catch (Exception ex)
+        {
+            lblMessages.Text = ex.Message;
+        }
+        Panel1.Visible = false;
+        gvAsiakkaat.Visible = true;
     }
 }
