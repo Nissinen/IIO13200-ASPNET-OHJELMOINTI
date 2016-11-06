5cc056ab515e681b4551fdffbf253a8afbcb0660
KoodiPankki <KoodiPankki@KOODIPANKKI-PC>
2016-11-06 15:17:17 +0200

    Teht 5 aloitus

---
+++ Tehtava5/Tehtava5.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Tehtava5.aspx.cs" Inherits="Tehtava5" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:SqlDataSource ID="srcAsiakas" runat="server" ConnectionString="<%$ ConnectionStrings:DemoxOyConnectionString %>" SelectCommand="SELECT * FROM [asiakas]"></asp:SqlDataSource>
+        <asp:Button ID="btnHaeKaikki" runat="server" Text="HaeKaikkiAsiakkaat" OnClick="btnHaeKaikki_Click" />
+        <asp:DropDownList ID="ddlCountries" runat="server"></asp:DropDownList>
+        <asp:GridView ID="gvAsiakkaat" runat="server">
+        </asp:GridView>
+        <asp:Label ID="lblMessages" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Tehtava5/Tehtava5.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tehtava5 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string ConnString = ConfigurationManager.ConnectionStrings["DemoxOyConnectionString"].ConnectionString;
+        string select = "Select maa from asiakas";
+        try
+        {
+            SqlConnection conn = new SqlConnection(ConnString);
+            SqlCommand MyCommand = new SqlCommand(select, conn);
+
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(MyCommand);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            ddlCountries.DataSource = dt;
+            ddlCountries.DataTextField = "maa";
+            ddlCountries.DataBind();
+            /*        var result = from c in dt select new { c.id, c.lastname };
+                //määritellään dropdownlistille mitä se esittää
+                ddlCountries.SelectedIndex = -1;
+                ddlCountries.DataSource = result.ToList();
+                ddlCountries.DataTextField = "lastname";
+                ddlCountries.DataValueField = "id";
+                ddlCountries.DataBind();
+                ddlCountries.Items.Insert(0, string.Empty);*/
+
+            gvAsiakkaat.DataSource = dt;
+            gvAsiakkaat.DataBind();
+            conn.Close();
+            da.Dispose();
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    protected void btnHaeKaikki_Click(object sender, EventArgs e)
+    {
+        string ConnString = ConfigurationManager.ConnectionStrings["DemoxOyConnectionString"].ConnectionString;
+        string select = "Select * from asiakas";
+        try
+        {
+            SqlConnection conn = new SqlConnection(ConnString);
+            SqlCommand MyCommand = new SqlCommand(select, conn);
+            
+            conn.Open();
+            SqlDataReader MyReader = MyCommand.ExecuteReader();
+            gvAsiakkaat.DataSource = MyReader;
+            gvAsiakkaat.DataBind();
+            conn.Close();
+        }
+        catch (Exception ex)
+        {
+            
+        }
+    }
+}
